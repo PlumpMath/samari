53a65b8108b5495b1f343a3b424086175a5b625f
johnlowry <johnlowry@sc.rr.com>
2015-11-22 15:47:02 -0500

    add

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/* js file */
+
+(function( window, undefined ){
+
+	
+    $(".a_tog").click(function(){
+        $(".a_tog").toggleClass("selector");
+    });
+
+})( window, undefined );
+
+(function( window, undefined ){
+
+	
+    $(".a_tog").click(function(){
+        $(".pic_tog").toggleClass("switchPic");
+    });
+
+})( window, undefined );
+
+(function( window, undefined ){
+
+	
+    $(".a_tog").click(function(){
+        $(".table_tog").toggleClass("switchTable");
+    });
+
+})( window, undefined );
